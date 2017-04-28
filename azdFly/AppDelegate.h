737df1dd29ba35b6ee51a2725b5fa0d3cd1d6f33
JianBinWu <121805186@qq.com>
2017-04-28 16:31:16 +0800

    Initial Commit

---
+++ azdFly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  azdFly
+//
+//  Created by 吴剑斌 on 2017/4/28.
+//  Copyright © 2017年 xmazd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
